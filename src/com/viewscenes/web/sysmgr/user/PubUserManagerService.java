264131886b4c3d32d2c24714504b0bc91df52895
liubin84 <liubin11@jd.com>
2019-09-04 18:10:29 +0800

    登录日志

--- src/com/viewscenes/web/sysmgr/user/PubUserManagerService.java
+++ src/com/viewscenes/web/sysmgr/user/PubUserManagerService.java
@@ -233,7 +233,10 @@ public class PubUserManagerService {
 		
 		String newid = "";
 		try {
-			
+			if(!isLetterDigit(pubUserBean.getUserPassword())) {
+				EXEException ex =  new EXEException("", "error:密码必须包含字母和数字，长度在6到12位之间", "");
+				return ex;	
+			}
 			boolean has = PubUserManagerServiceDao.isExistUser(pubUserBean);
 			
 			if (has){
@@ -252,7 +255,37 @@ public class PubUserManagerService {
 		}
 		return newid;
 	}
-	
+	/**
+	 * 包含大小写字母及数字且在6-12位
+	 * 是否包含
+	 *
+	 * @param str
+	 * @return
+	 */
+	public static boolean isLetterDigit(String str) {
+	    boolean isDigit = false;//定义一个boolean值，用来表示是否包含数字
+	    boolean isLetter = false;//定义一个boolean值，用来表示是否包含字母
+	    for (int i = 0; i < str.length(); i++) {
+	        if (Character.isDigit(str.charAt(i))) {   //用char包装类中的判断数字的方法判断每一个字符
+	            isDigit = true;
+	        } else if (Character.isLetter(str.charAt(i))) {  //用char包装类中的判断字母的方法判断每一个字符
+	            isLetter = true;
+	        }
+	    }
+	    String regex = "^[a-zA-Z0-9]{6,12}$";
+	    boolean isRight = isDigit && isLetter && str.matches(regex);
+	    return isRight;
+	}
+	public static void main(String[] arg){
+	    System.out.println(isLetterDigit(""));
+	    System.out.println(isLetterDigit("11111"));
+	    System.out.println(isLetterDigit("11111111"));
+	    System.out.println(isLetterDigit("aaa"));
+	    System.out.println(isLetterDigit("aaaaaaaaa"));
+	    System.out.println(isLetterDigit("a1"));
+	    System.out.println(isLetterDigit("a1111111111111"));
+	    System.out.println(isLetterDigit("a11111111111"));
+	}	
 	/**
 	 * 
 	 * <p>class/function:com.viewscenes.framework.service.pub
@@ -264,6 +297,11 @@ public class PubUserManagerService {
 	public ArrayList<Object> updateUser(PubUserBean pubUserBean){
 		ArrayList<Object> list =  new ArrayList<Object>();
 		try {
+			if(!isLetterDigit(pubUserBean.getUserPassword())) {
+				EXEException ex =  new EXEException("", "error:密码必须包含字母和数字，长度在6到12位之间", "");
+				list.add(ex);
+				return list;	
+			}
 			
 			String userName = pubUserBean.getUserName();
 			
